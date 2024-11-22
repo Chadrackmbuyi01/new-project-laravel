4192fed0ed3e940c26b050789570000488a45b22
Chadrack Mbuyi Kalala <80404784+Chadrackmbuyi01@users.noreply.github.com>
2024-11-22 13:26:58 +0200

    Creation of respective routes and separate concerns of controllers

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::post('/register', function(){
-    return 'Submission successful';
-});
+Route::post('/register', [UserController::class, 'register'])
