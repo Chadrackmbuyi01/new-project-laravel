0fb72ce5944013fc0c0cc9baf082da70f95915d5
Chadrack Mbuyi Kalala <80404784+Chadrackmbuyi01@users.noreply.github.com>
2024-11-26 18:32:50 +0200

    updating a new table on DB to test logging in with the existing user or registered one

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,11 +8,24 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function login(Request $request){
+        incomingFields = $request -> validate([
+            'login_name' => 'required',
+            'login_password' => 'required'
+        ]);
+
+        if (auth()-> attempt(['name' => $incomingFields['login_name', 'password' => $incomingFields['login_password']]])){
+            $request -> session()-> regenerate();
+
+        }
+        return redirect('/')
+
+    }
     public function logout(){
         auth()->logout();
         return redirect('/');
     }
-    
+
     public function register(Request $request){
         $incomingFields = $request -> validate([
             'name' => ['required', 'min:3', 'max:10', Rule::unique ('users','name')], // or as simple 'required',
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,6 +25,15 @@
             <button>Register</button>
         </form>
     </div>
+    <div style="border: 3px solid black;">
+        <h2>Login</h2>
+        <form action="/login" method="POST">
+        @csrf
+            <input name="login_name" type="text" placeholder="name">
+            <input name="login_password" type="password" placeholder="password">
+            <button>Log in</button>
+        </form>
+    </div>
     @endauth
     
 </body>
